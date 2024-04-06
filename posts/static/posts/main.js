0b7d9692f82814389664ef05c0d6401d7410fe9e
JAYGIRI GOSWAMI <rdj157003@gmail.com>
2024-04-06 17:43:25 -0400

    Adding sppiner to it

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -2,6 +2,7 @@ console.log("hello world")
 
 const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
 //helloWorldBox.innerHTML='hello <b>world</b>'
 
@@ -26,13 +27,28 @@ $.ajax({
     success: function(response){
         console.log(response)
         const data = response.data
-        console.log(data)
-        data.forEach(el => {
-            postsBox.innerHTML += `
-                ${el.title} - <b>${el.body}</b><br>
-            `
-                
-        });
+        setTimeout(() => {
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h5 class="card-title">${el.title}</h5>
+                            <p class="car-text">${el.body}</p>
+
+                        </div>
+                        <div class="card-footer">
+                            <a href= "#" class="btn btn-primary">Details</a>
+                         </div>
+                    </div>
+
+                `
+            });
+
+            
+        }, 1000)
+        
 
     },
     error: function(error){
