dc0ceb4ed07d519797dd2cf3ddf0f0de721f4050
JAYGIRI GOSWAMI <rdj157003@gmail.com>
2024-04-06 14:36:59 -0400

    Your commit message here

---
+++ posts/static/posts/main.js
@@ -0,0 +1,33 @@
+console.log("hello world")
+
+const helloWorldBox = document.getElementById('hello-world')
+
+//helloWorldBox.innerHTML='hello <b>world</b>'
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response.text)
+        helloWorldBox.textContent= response.text
+       
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+    
+
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+
+    },
+    error: function(error){
+        console.log(error)
+    }
+
+})
